634b110a4c96a1f4c5a5b630630a76c4fb608614
talha731 <118377080+talha731@users.noreply.github.com>
2023-01-13 18:37:49 +0000

    psudo coding done

--- starter/assets/JS/questions.js
+++ starter/assets/JS/questions.js
@@ -1,1 +1,7 @@
-// write 5 questions about javascipt
+// questions appers when start is clicked
+// there has to be 5 questions
+// timer is set for 2 mins
+//after answering a question another question is presented
+//when a question is answered incorrectly tikme is substracted
+// when all the questions are answered or teh timer reaches 0 the game is over
+// user is able to save their score with their initials
